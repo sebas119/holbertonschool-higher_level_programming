f89c1606d6e07117352f694a3957652e650e0ef9
sebas119 <lopez.sebastian@correunivalle.edu.co>
2019-12-10 22:04:30 +0000

    Uppercase letters

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+def uppercase(str):
+    for elem in str:
+        if (ord(elem) >= ord('a') and ord(elem) <= ord('z')):
+            elem = ord(elem) - ord(' ')
+        else:
+            elem = ord(elem)
+        print("{:c}".format(elem), end='')
+    print()
