bec354d738bff4cfbcfc4071918b44efe93df780
sebas119 <lopez.sebastian@correunivalle.edu.co>
2019-12-10 23:38:33 +0000

    Print the alphabet alternate upper and lower case

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,1 +1,6 @@
 #!/usr/bin/python3
+for i in range(ord('z'), ord('`'), -1):
+    if (i % 2 == 0):
+        print("{:c}".format(i), end='')
+    else:
+        print("{:c}".format(i - ord(' ')), end='')
