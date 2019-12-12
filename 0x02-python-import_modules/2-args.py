edc597e6c79393c449203c062e6282b61b66ca19
sebas119 <lopez.sebastian@correunivalle.edu.co>
2019-12-12 18:25:19 +0000

    Fix module

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,10 +3,11 @@
 import sys
 if __name__ == "__main__":
     argc = len(sys.argv) - 1
-    print("{:d} arguments".format(argc), end='')
     if argc == 0:
-        print(".")
+        print("0 arguments.")
+    elif argc == 1:
+        print("1 argument:")
     else:
-        print(":")
+        print("{:d} arguments:".format(argc))
     for i in range(1, len(sys.argv)):
         print("{:d}: {:s}".format(i, sys.argv[i]))
