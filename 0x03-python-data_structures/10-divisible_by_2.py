ebe17192b12ff62eb67a8932f391057d18c63d5c
sebas119 <lopez.sebastian@correunivalle.edu.co>
2019-12-17 18:51:13 +0000

    Divisible by 2

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+
+
+def divisible_by_2(my_list=[]):
+
+    new = []
+    if not my_list:
+        return new
+    for elem in my_list:
+        if elem % 2 == 0:
+            new.append(True)
+        else:
+            new.append(False)
+    return new
