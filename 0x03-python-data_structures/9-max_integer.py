e2fda68371c87fce269fb8c12fbddb4d0c503a66
sebas119 <lopez.sebastian@correunivalle.edu.co>
2019-12-17 18:44:37 +0000

    Max value in the list

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+
+
+def max_integer(my_list=[]):
+
+    if not my_list:
+        return None
+
+    m = my_list[0]
+    for elem in my_list:
+        if (elem > m):
+            m = elem
+    return m
