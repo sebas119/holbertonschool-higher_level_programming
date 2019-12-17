9ac57a1a58cb033a1ee2fe08406071ff42dd33fc
sebas119 <lopez.sebastian@correunivalle.edu.co>
2019-12-17 18:40:41 +0000

    Returns a tuple

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+
+
+def multiple_returns(sentence):
+
+    if len(sentence) == 0:
+        firstC = None
+    else:
+        firstC = sentence[0]
+    new = (len(sentence), firstC)
+    return new
