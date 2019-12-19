007acef9a8fc562ec0cca5410a695a96a3117633
sebas119 <lopez.sebastian@correunivalle.edu.co>
2019-12-19 18:27:42 +0000

    Returns a key with the biggest value 3

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -5,4 +5,5 @@ def best_score(a_dictionary):
 
     if not a_dictionary:
         return None
-    return max(a_dictionary)
+    big = max(a_dictionary.values())
+    return [k for k, v in a_dictionary.items() if big == v][0]
