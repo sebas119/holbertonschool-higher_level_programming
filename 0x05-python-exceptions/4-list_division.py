71236cc5b8f0828808fe581c03494cfabb71930c
sebas119 <lopez.sebastian@correunivalle.edu.co>
2020-01-08 17:00:47 +0000

    Divide a list

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,17 @@
+def list_division(my_list_1, my_list_2, list_length):
+    new = []
+    for i in range(0, list_length):
+        try:
+            ans = my_list_1[i] / my_list_2[i]
+            new.append(ans)
+        except ZeroDivisionError:
+            print("division by 0")
+            new.append(0)
+        except TypeError:
+            print("wrong type")
+            new.append(0)
+        except IndexError:
+            print("out of range")
+        finally:
+            pass
+    return new
