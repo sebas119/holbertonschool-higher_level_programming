0d1d2758347525b678c578e8b0a1300f5abfe687
sebas119 <lopez.sebastian@correunivalle.edu.co>
2020-01-13 12:34:38 +0000

    Update exercises

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python3
+
+
+def text_indentation(text):
+
+    if (not isinstance(text, str)):
+        raise TypeError("text must be a string")
+    text = text.replace(':', ':*&*').replace('?',
+                                             '?*&*').replace('.', '.*&*')
+    text = text.split('*&*')
+
+    new = []
+    for el in text:
+        new.append(el.strip())
+    for val in new:
+        if (val[-1] in {':', '?', '.'}):
+            print(val, end='\n\n')
+        else:
+            print(val, end='')
