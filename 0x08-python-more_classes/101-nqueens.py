7fe96094ef75a01de98e84a6f71174de30d6919b
sebas119 <lopez.sebastian@correunivalle.edu.co>
2020-01-14 23:29:27 +0000

    Starting queens

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+
+import sys
+
+argv = sys.argv
+
+if len(argv) != 2:
+    print("Usage: nqueens N")
+    sys.exit(1)
+
+n = int(argv[1])
+
+if not isinstance(n, int):
+    print("N must be a number")
+    sys.exit(1)
+if n < 4:
+    print("N must be at least 4")
+    sys.exit(1)
