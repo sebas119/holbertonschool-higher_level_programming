c5936a180c7a8249e2186300eacb8c10489d1d6c
sebas119 <lopez.sebastian@correunivalle.edu.co>
2020-01-15 16:04:26 +0000

    Fix typo

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -7,7 +7,7 @@ class Rectangle(object):
 
     @property
     def width(self):
-        return self.width
+        return self.__width
 
     @width.setter
     def width(self, value):
@@ -20,7 +20,7 @@ class Rectangle(object):
 
     @property
     def height(self):
-        return self.height
+        return self.__height
 
     @height.setter
     def height(self, value):
