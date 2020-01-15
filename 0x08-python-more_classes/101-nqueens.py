bea969952169ff00d04da02649ddf2141f7c71e0
sebas119 <lopez.sebastian@correunivalle.edu.co>
2020-01-15 04:31:35 +0000

    Update edge cases

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -1,18 +1,21 @@
 #!/usr/bin/python3
 
-import sys
+if __name__ == "__main__":
 
-argv = sys.argv
+    import sys
 
-if len(argv) != 2:
-    print("Usage: nqueens N")
-    sys.exit(1)
+    argv = sys.argv
 
-n = int(argv[1])
+    if len(argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
 
-if not isinstance(n, int):
-    print("N must be a number")
-    sys.exit(1)
-if n < 4:
-    print("N must be at least 4")
-    sys.exit(1)
+    try:
+        n = int(argv[1])
+    except Exception:
+        print("N must be a number")
+        sys.exit(1)
+
+    if n < 4:
+        print("N must be at least 4")
+        sys.exit(1)
