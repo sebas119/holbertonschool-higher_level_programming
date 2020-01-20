0edb2b82642f12c8fdbd0ee6e1ded462f599ca10
sebas119 <lopez.sebastian@correunivalle.edu.co>
2020-01-20 17:17:30 +0000

    Overwrite magic function __str__

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,1 +1,45 @@
 #!/usr/bin/python3
+#!/usr/bin/python3
+
+
+class BaseGeometry():
+
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+
+class Rectangle(BaseGeometry):
+
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
+
+    def __str__(self):
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
+
+    def area(self):
+        return self.__width * self.__height
+
+
+class Square(Rectangle):
+
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        self.__size = size
+        self._Rectangle__width = size
+        self._Rectangle__height = size
+
+    def area(self):
+        return self.__size ** 2
+
+    def __str__(self):
+        return "[Square] {}/{}".format(self.__size, self.__size)
