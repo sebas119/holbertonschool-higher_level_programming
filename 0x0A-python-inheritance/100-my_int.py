8d32aa9a690bc8854bb89e69fc83efdcee523da5
sebas119 <lopez.sebastian@correunivalle.edu.co>
2020-01-20 18:27:22 +0000

    Overwrite magic functions of int class __eq__ and __ne__

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+
+
+class MyInt(int):
+
+    def __eq__(self, value):
+        if (self.real == value):
+            return False
+        return True
+
+    def __ne__(self, value):
+        if (self.real != value):
+            return False
+        return True
