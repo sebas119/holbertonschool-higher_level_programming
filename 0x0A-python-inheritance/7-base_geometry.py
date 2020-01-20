f8c2a25717007a5d11da70117a2be6f4fb15de9b
sebas119 <lopez.sebastian@correunivalle.edu.co>
2020-01-20 17:01:35 +0000

    Rectangle object and some fix conditionals

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -8,7 +8,7 @@ class BaseGeometry():
 
     def integer_validator(self, name, value):
 
-        if not isinstance(value, int):
-            raise TypeError("<name> must be an integer")
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(name))
         if value <= 0:
-            raise ValueError("<name> must be greater than 0")
+            raise ValueError("{} must be greater than 0".format(name))
