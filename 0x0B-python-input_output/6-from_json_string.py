cda17dc23a618628c0d3dab43eee2eb83b6bae59
sebas119 <lopez.sebastian@correunivalle.edu.co>
2020-01-21 20:36:03 +0000

    Function that returns an object represented by a JSON string

--- 0x0B-python-input_output/6-from_json_string.py
+++ 0x0B-python-input_output/6-from_json_string.py
@@ -1,2 +1,10 @@
 #!/usr/bin/python3
+
+"""String to JSON module"""
+
+import json
+
+
 def from_json_string(my_str):
+    """Function that returns a object represented by a JSON string"""
+    return json.loads(my_str)
