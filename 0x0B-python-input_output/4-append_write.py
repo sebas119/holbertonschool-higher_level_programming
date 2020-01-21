87e7663d75d00736ddc2cfc8e65492d0fecef21e
sebas119 <lopez.sebastian@correunivalle.edu.co>
2020-01-21 19:58:19 +0000

    Append text at the end of the file

--- 0x0B-python-input_output/4-append_write.py
+++ 0x0B-python-input_output/4-append_write.py
@@ -1,2 +1,9 @@
 #!/usr/bin/python3
+"""Append text module"""
+
+
 def append_write(filename="", text=""):
+    """Function that appends text at the end of the file"""
+
+    with open(filename, mode='a', encoding="UTF-8") as myfile:
+        return myfile.write(text)
