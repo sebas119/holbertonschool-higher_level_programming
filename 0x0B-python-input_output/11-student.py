d5a706fa22c000c458277f5c8370370ee953308b
sebas119 <lopez.sebastian@correunivalle.edu.co>
2020-01-21 23:21:37 +0000

    Student class

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+
+
+class Student:
+
+    def __init__(self, first_name, last_name, age):
+        """Init method"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+
+        return self.__dict__
