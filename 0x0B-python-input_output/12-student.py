529fae6066f57dd729fc9a2a3c5108ef6b1ca8ae
sebas119 <lopez.sebastian@correunivalle.edu.co>
2020-01-21 23:28:52 +0000

    Student to json with filter

--- 0x0B-python-input_output/12-student.py
+++ 0x0B-python-input_output/12-student.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+
+
+class Student:
+
+    def __init__(self, first_name, last_name, age):
+        """Init method"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+
+        if (isinstance(attrs, list) and
+                all(isinstance(el, str) for el in attrs)):
+            return {i: getattr(self, i) for i in attrs if hasattr(self, i)}
+        return self.__dict__
