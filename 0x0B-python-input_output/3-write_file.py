1f2d7f444788c8d98439709ac122ce3831d6243b
sebas119 <lopez.sebastian@correunivalle.edu.co>
2020-01-21 19:50:22 +0000

    Write lines in a file

--- 0x0B-python-input_output/3-write_file.py
+++ 0x0B-python-input_output/3-write_file.py
@@ -1,2 +1,10 @@
 #!/usr/bin/python3
+"""Write line module"""
+
+
 def write_file(filename="", text=""):
+    """Function that write lines in a file"""
+
+    with open(filename, mode='w', encoding="UTF-8") as myfile:
+
+        return myfile.write(text)
