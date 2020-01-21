657a7bd0217e181be5095377745e2be8b942339e
sebas119 <lopez.sebastian@correunivalle.edu.co>
2020-01-21 19:46:06 +0000

    Read lines specific in files

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,4 +4,4 @@
 def read_file(filename=""):
 
     with open(filename, mode="r", encoding="UTF-8") as myfile:
-        print(myfile.read())
+        print(myfile.read(), end='')
--- 0x0B-python-input_output/2-read_lines.py
+++ 0x0B-python-input_output/2-read_lines.py
@@ -1,2 +1,13 @@
 #!/usr/bin/python3
+
+
 def read_lines(filename="", nb_lines=0):
+
+    with open(filename, mode="r", encoding="UTF-8") as myfile:
+
+        if nb_lines <= 0 or nb_lines >= len(myfile.readlines()):
+            print(myfile.read(), end='')
+        else:
+            myfile.seek(0)
+            for i in range(nb_lines):
+                print(myfile.readline(), end='')
