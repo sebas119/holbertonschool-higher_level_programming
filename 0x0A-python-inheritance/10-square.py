798cece29ef447cb7d15cb0426df5c78f4fb5cc3
sebas119 <lopez.sebastian@correunivalle.edu.co>
2020-01-21 05:31:19 +0000

    Call the constructor from the parent class with super()

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -34,8 +34,7 @@ class Square(Rectangle):
     def __init__(self, size):
         self.integer_validator("size", size)
         self.__size = size
-        self._Rectangle__width = size
-        self._Rectangle__height = size
+        super().__init__(size, size)
 
     def area(self):
         return self.__size ** 2
