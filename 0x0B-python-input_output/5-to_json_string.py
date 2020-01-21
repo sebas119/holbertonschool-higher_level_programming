922075f6136432149686bad24515e5999a1f6cbe
sebas119 <lopez.sebastian@correunivalle.edu.co>
2020-01-21 20:04:02 +0000

    Serialize an obj python to a JSON formatted str

--- 0x0B-python-input_output/5-to_json_string.py
+++ 0x0B-python-input_output/5-to_json_string.py
@@ -1,2 +1,11 @@
 #!/usr/bin/python3
+"""Json to string module"""
+
+import json
+
+
 def to_json_string(my_obj):
+    """Function that return the JSON representation of
+    an object"""
+
+    return json.dumps(my_obj)
