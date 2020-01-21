a893cde6b648d846f4ff9439b6d9972fdbe86882
sebas119 <lopez.sebastian@correunivalle.edu.co>
2020-01-21 20:41:40 +0000

    Save object representation of JSON file

--- 0x0B-python-input_output/7-save_to_json_file.py
+++ 0x0B-python-input_output/7-save_to_json_file.py
@@ -1,2 +1,13 @@
 #!/usr/bin/python3
+
+"""object JSON to a file module"""
+
+import json
+
+
 def save_to_json_file(my_obj, filename):
+    """Save object representation of JSON to a file"""
+
+    with open(filename, mode='w', encoding='UTF-8') as myfile:
+
+        json.dump(my_obj, myfile)
