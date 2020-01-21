04f615894e1f8d87594e2e5bd156b0e8d38119a6
sebas119 <lopez.sebastian@correunivalle.edu.co>
2020-01-21 23:05:00 +0000

    Count lines updated

--- 0x0B-python-input_output/2-read_lines.py
+++ 0x0B-python-input_output/2-read_lines.py
@@ -5,7 +5,8 @@ def read_lines(filename="", nb_lines=0):
 
     with open(filename, mode="r", encoding="UTF-8") as myfile:
 
-        if nb_lines <= 0 or nb_lines >= len(myfile.readlines()):
+        if nb_lines <= 0 or nb_lines >= sum(1 for line in myfile):
+            myfile.seek(0)
             print(myfile.read(), end='')
         else:
             myfile.seek(0)
