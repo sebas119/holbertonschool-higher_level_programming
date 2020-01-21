9865d95676dc20819112263fb93f0990db0814dc
sebas119 <lopez.sebastian@correunivalle.edu.co>
2020-01-21 05:25:03 +0000

    Comments MyList

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 
+"""Defines a subclass inherited from list."""
+
 
 class MyList(list):
+    """Class MyList"""
 
     def print_sorted(self):
+        """Print a list in sorted ascending order."""
         print(sorted(self))
