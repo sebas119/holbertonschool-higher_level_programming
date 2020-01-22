e35024188e71f90a88454f9ce34bf1d523028fad
sebas119 <lopez.sebastian@correunivalle.edu.co>
2020-01-22 00:20:06 +0000

    Pascal triangle solution

--- 0x0B-python-input_output/14-pascal_triangle.py
+++ 0x0B-python-input_output/14-pascal_triangle.py
@@ -1,1 +1,24 @@
 #!/usr/bin/python3
+
+
+def pascal_triangle(n):
+    """Returns a list of lists of integers representing the
+    Pascal’s triangle of n"""
+    if n <= 0:
+        return []
+
+    triangle = []
+    firstRow = [1]
+    triangle.append(firstRow)
+
+    for i in range(1, n):
+        prev = triangle[i - 1]
+        row = []
+        row.append(1)
+
+        for j in range(1, i):
+            row.append(prev[j - 1] + prev[j])
+        row.append(1)
+        triangle.append(row)
+
+    return triangle
