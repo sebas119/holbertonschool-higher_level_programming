2768c19e67aecbaf2b1e7b44f40803d946828ac7
sebas119 <lopez.sebastian@correunivalle.edu.co>
2020-01-23 21:23:54 +0000

    Area first

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -79,3 +79,8 @@ class Rectangle(Base):
         if value < 0:
             raise ValueError("y must be >= 0")
         self.__y = value
+
+    def area(self):
+        """Returns the area Rectangle"""
+
+        return self.__width * self.__height
