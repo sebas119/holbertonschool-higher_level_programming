a64118ed6d5972fa5766dfdc95da7d7b27ebf581
sebas119 <lopez.sebastian@correunivalle.edu.co>
2020-01-23 19:47:49 +0000

    Base class

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+
+"""Defines a class Base.""""
+
+
+class Base:
+    """Represent a Base."""
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """Initialize a new Base"""
+
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
