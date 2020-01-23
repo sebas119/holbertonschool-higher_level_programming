f1a5f546fcb4c52fa4f94f540fc2a719b0862f5d
sebas119 <lopez.sebastian@correunivalle.edu.co>
2020-01-23 21:28:02 +0000

    Display #0

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -84,3 +84,11 @@ class Rectangle(Base):
         """Returns the area Rectangle"""
 
         return self.__width * self.__height
+
+    def display(self):
+        """Prints in stdout the Rectangle instance with the character #"""
+
+        for i in range(self.__height):
+            for j in range(self.__width):
+                print('#', end='')
+            print()
