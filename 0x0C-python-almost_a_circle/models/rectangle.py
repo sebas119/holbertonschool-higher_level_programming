934b32486beaaa89bb143e49319ce9d9b510769b
sebas119 <lopez.sebastian@correunivalle.edu.co>
2020-01-23 21:48:15 +0000

    __str__

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -92,3 +92,14 @@ class Rectangle(Base):
             for j in range(self.__width):
                 print('#', end='')
             print()
+
+    def __str__(self):
+        """String representation of the class"""
+
+        return "[Rectangle] ({}) {}/{} - <{}>/<{}>".format(
+            self.id,
+            self.x,
+            self.y,
+            self.width,
+            self.height
+        )
