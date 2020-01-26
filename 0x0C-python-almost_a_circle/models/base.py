0732a37c11c75e44dd6efa63de137b490fd7a351
sebas119 <lopez.sebastian@correunivalle.edu.co>
2020-01-26 00:45:36 +0000

    JSON ok, but CSV? exercise

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -79,7 +79,7 @@ class Base:
         try:
             with open(cls.__name__ + ".json", 'r') as file:
                 data = cls.from_json_string(file.read())
-        except FileNotFoundError:
+        except IOError:
             return list_instances
 
         for el in data:
@@ -95,17 +95,78 @@ class Base:
         * Square: <id>,<size>,<x>,<y>
         """
 
-        filename = cls.__name__
-        for el in list_objs:
-            print(cls.__name__ == "Rectangle")
-            print(cls.__name__ == "Square")
-            for key, value in el.to_dictionary().items():
-                pass
+        with open(cls.__name__ + ".csv", "w") as mycsv:
+            if list_objs is None or list_objs == []:
+                mycsv.write("[]")
+            else:
+                list_data = []
+                for el in list_objs:
+                    if cls.__name__ == "Rectangle":
+                        data = ["id", "width", "height", "x", "y"]
+                    elif cls.__name__ == "Square":
+                        data = ["id", "size", "x", "y"]
+                    for key, value in el.to_dictionary().items():
+                        if len(data) == 5:
+                            if key == "id":
+                                data[0] = str(value)
+                            elif key == "width":
+                                data[1] = str(value)
+                            elif key == "height":
+                                data[2] = str(value)
+                            elif key == "x":
+                                data[3] = str(value)
+                            elif key == "y":
+                                data[4] = str(value)
+                        elif len(data) == 4:
+                            if key == "id":
+                                data[0] = str(value)
+                            elif key == "size":
+                                data[1] = str(value)
+                            elif key == "x":
+                                data[2] = str(value)
+                            elif key == "y":
+                                data[3] = str(value)
+                    data = ",".join([str(el) for el in data])
+                    list_data.append(data)
+                for el in list_data:
+                    mycsv.write(el)
+                    mycsv.write("\n")
 
     @classmethod
     def load_from_file_csv(cls):
         """
-
+        Loads the list_objs from a file in this format:
+        * Rectangle: <id>,<width>,<height>,<x>,<y>
+        * Square: <id>,<size>,<x>,<y>
         """
+        try:
+            with open(cls.__name__ + ".csv", "r") as mycsv:
+                raw_data = mycsv.readlines()
+                list_data = []
+                for el in raw_data:
+                    new = el.replace('\n', '')
+                    list_data.append(new)
+                list_instances = []
+                for el in list_data:
+                    clean_list = el.split(',')
+                    if cls.__name__ == "Rectangle":
+                        data = {"id": 0, "width": 0,
+                                "height": 0, "x": 0, "y": 0}
+                    elif cls.__name__ == "Square":
+                        data = {"id": 0, "size": 0, "x": 0, "y": 0}
+                    if len(clean_list) == 5:
+                        data["id"] = int(clean_list[0])
+                        data["width"] = int(clean_list[1])
+                        data["height"] = int(clean_list[2])
+                        data["x"] = int(clean_list[3])
+                        data["y"] = int(clean_list[4])
+                    elif len(clean_list) == 4:
+                        data["id"] = int(clean_list[0])
+                        data["size"] = int(clean_list[1])
+                        data["x"] = int(clean_list[2])
+                        data["y"] = int(clean_list[3])
+                    list_instances.append(cls.create(**data))
 
-        filename = cls.__name__
+                return list_instances
+        except IOError:
+            return []
