23a6ff36eadcb63c17840ce193d2f7847d7ea056
sebas119 <lopez.sebastian@correunivalle.edu.co>
2020-03-03 23:16:15 +0000

    Remove specific use statement to select the database

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,5 +1,4 @@
 -- Script that lists all the cities of California that can be found in the database hbtn_0d_usa.
-USE `hbtn_0d_usa`;
 SELECT cities.id `id`, cities.name `name` FROM cities
     WHERE state_id = (SELECT states.id `id` FROM states WHERE states.name = 'California')
     ORDER BY cities.id;
