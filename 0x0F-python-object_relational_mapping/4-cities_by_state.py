2bb754289c5896592403533dbe9711d4c735c6cd
Sebastian Lopez H <lopez.sebastian@correounivalle.edu.co>
2020-03-27 20:50:00 +0000

    Script that lists all cities from the database hbtn_0e_4_usa

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,1 +1,30 @@
 #!/usr/bin/python3
+
+"""
+Script that lists all cities from the database hbtn_0e_4_usa
+"""
+
+if __name__ == "__main__":
+    import sys
+
+    if len(sys.argv) > 3:
+        import MySQLdb
+
+        username = sys.argv[1]
+        password = sys.argv[2]
+        db_name = sys.argv[3]
+
+        conn = MySQLdb.connect(host="localhost", port=3306, user=username,
+                               passwd=password, db=db_name, charset="utf8")
+        cur = conn.cursor()
+        cur.execute("""
+            SELECT cities.id, cities.name, states.name FROM cities
+            INNER JOIN states
+            ON cities.state_id = states.id
+            ORDER BY cities.id
+        """)
+        query_rows = cur.fetchall()
+        for row in query_rows:
+            print(row)
+        cur.close()
+        conn.close()
