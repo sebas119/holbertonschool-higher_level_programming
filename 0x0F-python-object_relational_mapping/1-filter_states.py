545427870a00909d3654ebaed9ddd7c5a636f43a
Sebastian Lopez H <lopez.sebastian@correounivalle.edu.co>
2020-03-27 19:59:39 +0000

    Fix code should not be executed when imported

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -2,22 +2,25 @@
 
 """Script that lists all states with a name starting with N
 """
-import sys
 
-if len(sys.argv) > 3:
-    import MySQLdb
+if __name__ == "__main__":
+        
+    import sys
 
-    username = sys.argv[1]
-    password = sys.argv[2]
-    db_name = sys.argv[3]
+    if len(sys.argv) > 3:
+        import MySQLdb
 
-    conn = MySQLdb.connect(host="localhost", port=3306, user=username,
-                           passwd=password, db=db_name, charset="utf8")
-    cur = conn.cursor()
-    cur.execute(
-        "SELECT * FROM states WHERE states.name LIKE 'N%' ORDER BY states.id")
-    query_rows = cur.fetchall()
-    for row in query_rows:
-        print(row)
-    cur.close()
-    conn.close()
+        username = sys.argv[1]
+        password = sys.argv[2]
+        db_name = sys.argv[3]
+
+        conn = MySQLdb.connect(host="localhost", port=3306, user=username,
+                            passwd=password, db=db_name, charset="utf8")
+        cur = conn.cursor()
+        cur.execute(
+            "SELECT * FROM states WHERE states.name LIKE 'N%' ORDER BY states.id")
+        query_rows = cur.fetchall()
+        for row in query_rows:
+            print(row)
+        cur.close()
+        conn.close()
--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -4,24 +4,26 @@
 script that takes in an argument and displays all values
 in the states table of hbtn_0e_0_usa where name matches the argument.
 """
-import sys
 
-if len(sys.argv) > 4:
-    import MySQLdb
+if __name__ == "__main__":
+    import sys
 
-    username = sys.argv[1]
-    password = sys.argv[2]
-    db_name = sys.argv[3]
-    state_searched = sys.argv[4]
+    if len(sys.argv) > 4:
+        import MySQLdb
 
-    conn = MySQLdb.connect(host="localhost", port=3306, user=username,
-                           passwd=password, db=db_name, charset="utf8")
-    cur = conn.cursor()
-    sql = "SELECT * FROM states WHERE name = '{:s}' ORDER BY states.id".format(
-        state_searched)
-    cur.execute(sql)
-    query_rows = cur.fetchall()
-    for row in query_rows:
-        print(row)
-    cur.close()
-    conn.close()
+        username = sys.argv[1]
+        password = sys.argv[2]
+        db_name = sys.argv[3]
+        state_searched = sys.argv[4]
+
+        conn = MySQLdb.connect(host="localhost", port=3306, user=username,
+                            passwd=password, db=db_name, charset="utf8")
+        cur = conn.cursor()
+        sql = "SELECT * FROM states WHERE name = '{:s}' ORDER BY states.id".format(
+            state_searched)
+        cur.execute(sql)
+        query_rows = cur.fetchall()
+        for row in query_rows:
+            print(row)
+        cur.close()
+        conn.close()
