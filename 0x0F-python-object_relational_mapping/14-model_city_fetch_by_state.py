4456000254f71c142a2af52cdccb7043864f74ad
Sebastian Lopez H <lopez.sebastian@correounivalle.edu.co>
2020-03-28 19:31:40 +0000

    Script 14-model_city_fetch_by_state.py that prints all City objects from the database hbtn_0e_14_usa

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+"""Script 14-model_city_fetch_by_state.py that prints all City
+objects from the database hbtn_0e_14_usa
+"""
+import sys
+from model_state import Base, State
+from model_city import City
+
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import Session
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    session = Session(engine)
+    query = session.query(City, State).select_from(City).join(
+        State, City.state_id == State.id).order_by(City.id).all()
+    for city, state in query:
+        print("{}: ({}) {}".format(state.name, city.id, city.name))
+    Base.metadata.create_all(engine)
+    session.close()
--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+
+"""Model city module
+"""
+
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class City(Base):
+    """Represents a city"""
+    __tablename__ = 'cities'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
