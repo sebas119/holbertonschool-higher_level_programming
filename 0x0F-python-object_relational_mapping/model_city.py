2100857411ab8c11683fdc94b083d1c6bd189991
Sebastian Lopez H <lopez.sebastian@correounivalle.edu.co>
2020-03-28 19:42:00 +0000

    Imported fixed

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -4,9 +4,7 @@
 """
 
 from sqlalchemy import Column, Integer, String, ForeignKey
-from sqlalchemy.ext.declarative import declarative_base
-
-Base = declarative_base()
+from model_state import Base
 
 
 class City(Base):
