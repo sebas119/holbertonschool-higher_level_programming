381a89225b0c14f901fffd39c1f93ff9273ed739
Sebastian Lopez H <lopez.sebastian@correounivalle.edu.co>
2020-03-28 01:36:42 +0000

    Script that prints the first State object from the database hbtn_0e_6_usa

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+"""Script that prints the first State object from the database hbtn_0e_6_usa
+"""
+import sys
+from model_state import State
+
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import Session
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    session = Session(engine)
+    state = session.query(State).order_by(State.id).first()
+    print("{}: {}".format(state.id, state.name))
+    session.close()
