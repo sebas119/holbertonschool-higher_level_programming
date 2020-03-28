97a11e4d5e6beed981a826e98b4723287af2b430
Sebastian Lopez H <lopez.sebastian@correounivalle.edu.co>
2020-03-28 17:33:15 +0000

    Binary comparasion fixed

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -23,7 +23,7 @@ if __name__ == "__main__":
             SELECT cities.name FROM cities
             INNER JOIN states
             ON cities.state_id = states.id
-            WHERE states.name = %s
+            WHERE BINARY states.name = %s
             ORDER BY cities.id
         """, (state_name, ))
         query_rows = cur.fetchall()
