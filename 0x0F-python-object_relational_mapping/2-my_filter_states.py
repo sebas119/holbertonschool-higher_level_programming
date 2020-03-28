666252d88edd4c27bd0124a83657261c54a33a1b
Sebastian Lopez H <lopez.sebastian@correounivalle.edu.co>
2020-03-28 17:21:46 +0000

    Shorter docstring

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,30 +1,27 @@
 #!/usr/bin/python3
 
+"""script that get all values that where name matches the argument
 """
-script that takes in an argument and displays all values
-in the states table of hbtn_0e_0_usa where name matches the argument.
-"""
+
+import sys
+import MySQLdb
 
 if __name__ == "__main__":
-    import sys
 
-    if len(sys.argv) > 4:
-        import MySQLdb
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+    state_searched = sys.argv[4]
 
-        username = sys.argv[1]
-        password = sys.argv[2]
-        db_name = sys.argv[3]
-        state_searched = sys.argv[4]
-
-        conn = MySQLdb.connect(host="localhost", port=3306, user=username,
-                               passwd=password, db=db_name, charset="utf8")
-        cur = conn.cursor()
-        cur.execute(
-            """SELECT * FROM states
-            WHERE name = %s
-            ORDER BY states.id""", (state_searched, ))
-        query_rows = cur.fetchall()
-        for row in query_rows:
-            print(row)
-        cur.close()
-        conn.close()
+    conn = MySQLdb.connect(host="localhost", port=3306, user=username,
+                           passwd=password, db=db_name, charset="utf8")
+    cur = conn.cursor()
+    cur.execute(
+        """SELECT * FROM states
+        WHERE name = %s
+        ORDER BY states.id""", (state_searched, ))
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
