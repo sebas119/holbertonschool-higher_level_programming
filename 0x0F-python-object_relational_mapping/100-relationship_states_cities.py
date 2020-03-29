0016994da8e9dff5d80a74a4ac8cb45cd654254d
Sebastian Lopez H <lopez.sebastian@correounivalle.edu.co>
2020-03-29 04:09:37 +0000

    Create_all() method updated

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-"""Script that adds the State object “Louisiana”
-to the database hbtn_0e_6_usa
+"""Script that creates the State “California” with the City “San Francisco”
+from the database hbtn_0e_100_usa
 """
 import sys
 from relationship_state import Base, State
@@ -12,8 +12,9 @@ from sqlalchemy.orm import Session
 if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
         sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    # Create all the objects into mysql
+    Base.metadata.create_all(engine)
     session = Session(engine)
     session.add(City(name="San Francisco", state=State(name="California")))
     session.commit()
-    Base.metadata.create_all(engine)
     session.close()
