2410a0a72350ffadada718b961f884b1dfcff161
Sebastian Lopez H <lopez.sebastian@correounivalle.edu.co>
2020-04-07 00:02:19 +0000

    Sends a POST request to the passed URL, and displays the body of the response

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,1 +1,3 @@
 #!/bin/bash
+# Sends a POST request to the passed URL, and displays the body of the response
+curl -d "email=hr@holbertonschool.com&subject=I will always be here for PLD" -X POST "$1"
