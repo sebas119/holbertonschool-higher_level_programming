8c9a61aca0bb058153b307c533eab23959ad9363
Sebastian Lopez H <lopez.sebastian@correounivalle.edu.co>
2020-04-09 20:55:17 +0000

    Silent mode in case of errors

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # sends a JSON POST request to a URL passed as the first argument, and displays the body of the response.
-curl -H "Content-Type: application/json" -X POST -d "$(cat "$2")" "$1"
+curl -s -H "Content-Type: application/json" -X POST -d "$(cat "$2")" "$1"
