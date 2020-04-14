7bf6a243ffe2d5fb0afe006cc442957c00bb3d43
Sebastian Lopez H <lopez.sebastian@correounivalle.edu.co>
2020-04-14 18:22:40 +0000

    Fix pep8

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 
-"""script that takes in a URL and an email, sends a POST request to the passed URL with
-the email as a parameter, and displays the body of the response"""
+"""script that takes in a URL and an email, sends a POST request
+to the passed URL with the email as a parameter, and displays the
+body of the response"""
 
 if __name__ == "__main__":
     from urllib import request, parse
--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
-"""script that takes in a URL, sends a request to the URL and displays the body of the response"""
+"""script that takes in a URL, sends a request to the URL
+and displays the body of the response"""
 
 if __name__ == "__main__":
     from urllib import request, error
