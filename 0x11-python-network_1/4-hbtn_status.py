7c67b3bfadb27bde184f3707fb516ec1c8ea3453
Sebastian Lopez H <lopez.sebastian@correounivalle.edu.co>
2020-04-14 18:29:43 +0000

    Basic request with package requests of python

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+
+"""script that fetches https://intranet.hbtn.io/status"""
+
+if __name__ == "__main__":
+    import requests
+
+    r = requests.get('https://intranet.hbtn.io/status')
+    print("Body response:")
+    print("    - type: {}".format(type(r.text)))
+    print("    - content: {}".format(r.text))
