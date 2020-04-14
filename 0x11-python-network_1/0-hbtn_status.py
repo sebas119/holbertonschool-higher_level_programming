29feb0d3f1c5d5a97ddc854c8e279a675d52704b
Sebastian Lopez H <lopez.sebastian@correounivalle.edu.co>
2020-04-14 16:36:52 +0000

    Python script that fetch the body response of a web with urllib

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+
+# Python script that fetches https://intranet.hbtn.io/status
+
+if __name__ == "__main__":
+    import urllib.request
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as f:
+        body = f.read()
+        print("Body response:")
+        print("    - type: {}".format(type(body)))
+        print("    - content: {}".format(body))
+        print("    - utf8 content: {}".format(body.decode('utf-8')))
