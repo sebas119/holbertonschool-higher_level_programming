295290da0806158f3daefac954fe34587424a25e
Sebastian Lopez H <lopez.sebastian@correounivalle.edu.co>
2020-05-11 22:31:06 +0000

    Sorted ocurrences

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,1 +1,15 @@
 #!/usr/bin/node
+
+const dict = require('./101-data').dict;
+
+const ans = {};
+
+for (const key in dict) {
+  if (ans[dict[key]] === undefined) {
+    ans[dict[key]] = [key];
+  } else {
+    ans[dict[key]].push(key);
+  }
+}
+
+console.log(ans);
