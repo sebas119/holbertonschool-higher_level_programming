10b8724fdcb39d95186634db1abb160cfff5ae56
Sebastian Lopez H <lopez.sebastian@correounivalle.edu.co>
2020-05-11 20:29:43 +0000

    Square charprint()

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,1 +1,15 @@
 #!/usr/bin/node
+
+const SquareFather = require('./5-square');
+
+module.exports = class Square extends SquareFather {
+  charPrint (c) {
+    if (c === undefined) {
+      this.print();
+    } else {
+      for (let i = 0; i < this.height; i++) {
+        console.log(c.repeat(this.width));
+      }
+    }
+  }
+};
