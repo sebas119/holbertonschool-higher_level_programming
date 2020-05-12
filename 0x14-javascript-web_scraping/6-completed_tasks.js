cbc0e6c19aca83bbc0ffb27280bb015a62aef138
Sebastian Lopez H <lopez.sebastian@correounivalle.edu.co>
2020-05-12 23:33:41 +0000

    Script that computes the number of tasks completed by user id

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,1 +1,20 @@
 #!/usr/bin/node
+
+const request = require('request');
+
+request.get(process.argv[2], (error, response, body) => {
+  if (error) {
+    console.log(error);
+  } else {
+    const todos = JSON.parse(body);
+    const ans = {};
+    for (const todo of todos) {
+      if (todo.completed === true && ans[todo.userId] === undefined) {
+        ans[todo.userId] = 1;
+      } else if (todo.completed === true) {
+        ans[todo.userId] += 1;
+      }
+    }
+    console.log(ans);
+  }
+});
