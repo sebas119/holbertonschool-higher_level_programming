b5d7d0468ca6a539377985355e9a77a3e3d48999
Sebastian Lopez H <lopez.sebastian@correounivalle.edu.co>
2020-05-12 23:06:49 +0000

    prints the number of movies where the character “Wedge Antilles” is present.

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,1 +1,22 @@
 #!/usr/bin/node
+
+const request = require('request');
+
+request(process.argv[2], (error, response, body) => {
+  if (error) {
+    console.log(error);
+  } else {
+    if (response && response.statusCode === 200) {
+      const results = JSON.parse(body).results;
+      let counter = 0;
+      let found;
+      for (const data of results) {
+        found = data.characters.find((n) => {
+          return n.endsWith('/18/');
+        });
+        if (found !== undefined) counter++;
+      }
+      console.log(counter);
+    }
+  }
+});
