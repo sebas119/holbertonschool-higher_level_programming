e31d980507bdaf8d248e83c95278f01ebb86d1b0
Sebastian Lopez H <lopez.sebastian@correounivalle.edu.co>
2020-05-12 23:15:09 +0000

    script that gets the contents of a webpage and stores it in a file

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,1 +1,18 @@
 #!/usr/bin/node
+
+const fs = require('fs');
+const request = require('request');
+
+request.get(process.argv[2], (error, response, body) => {
+  if (error) {
+    console.log(error);
+  } else {
+    if (response && response.statusCode === 200) {
+      fs.writeFile(process.argv[3], body, (err, data) => {
+        if (err) {
+          console.log(err);
+        }
+      });
+    }
+  }
+});
