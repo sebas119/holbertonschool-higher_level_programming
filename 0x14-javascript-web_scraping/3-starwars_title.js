b19f549f50649c591f2e036ae5c44922aaa9dce9
Sebastian Lopez H <lopez.sebastian@correounivalle.edu.co>
2020-05-12 22:44:50 +0000

    Title of star wars movie

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,1 +1,14 @@
 #!/usr/bin/node
+
+const request = require('request');
+const movieId = process.argv[2];
+
+request(`https://swapi-api.hbtn.io/api/films/${movieId}`, (error, response, body) => {
+  if (error) {
+    console.log(error);
+  } else {
+    if (response && response.statusCode === 200) {
+      console.log(JSON.parse(body).title);
+    }
+  }
+});
