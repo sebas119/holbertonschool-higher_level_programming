c5ee854e4a6f576c50a78e3f8477a37c568cb5cd
Sebastian Lopez H <lopez.sebastian@correounivalle.edu.co>
2020-05-13 04:10:19 +0000

    Print names characters of given film of start wars

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,1 +1,25 @@
 #!/usr/bin/node
+
+const request = require('request');
+
+const opCharacters = {
+  url: `https://swapi-api.hbtn.io/api/films/${process.argv[2]}`
+};
+
+request.get(opCharacters, (error, response, body) => {
+  if (!error) {
+    const charUrls = JSON.parse(body).characters;
+    printNames(charUrls, 0);
+  }
+});
+
+const printNames = (charUrls, index) => {
+  request.get(charUrls[index], (error, response, body) => {
+    if (!error) {
+      console.log(JSON.parse(body).name);
+      if (index + 1 < charUrls.length) {
+        printNames(charUrls, index + 1);
+      }
+    }
+  });
+};
